dd107dc48ab75c3c8104b6bbd26694d10956a0d4
Kezuv <armin.foucek@gmail.com>
2022-10-05 20:48:41 +0200

    1. Zwischenspeicherung

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,16 +6,56 @@ public class App {
 
     //todo Task 1
     public void largestNumber(){
+        Scanner scanner = new Scanner(System.in);
+        float numb;
+        int count = 1;
+        float biggest = 0;
+
+        System.out.print("Number " + count +": ");
+        numb = scanner.nextFloat();
+
+        if (numb > 0) {
+            do {
+                count++;
+                System.out.print("Number " + count +": ");
+                numb = scanner.nextFloat();
+                if (numb > biggest){
+                    biggest = numb;
+                }
+            } while (numb > 0);
+            System.out.print("The largest number is ");
+            System.out.printf("%.2f",biggest);
+            System.out.println();
+
+        } else {
+            System.out.println("No number entered.");
+        }
         // input your solution here
     }
 
     //todo Task 2
     public void stairs(){
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("n: ");
+            int n = scanner.nextInt();
+            int count  =1;
+
+            for (int i=1; i <= n ; i ++){
+                for (int j = 1; j<=i;j++){
+                    System.out.print(count+" ");
+                    count++;
+                }
+                 System.out.println();
+            }
         // input your solution here
     }
 
     //todo Task 3
     public void printPyramid(){
+        final int ROWS = 6;
+        final int COLS = 11;
+
+
         // input your solution here
     }
 
